b212be0b1e7c930dd04d9115db0486cc1381d1eb
HenSovatra <138690483+HenSovatra@users.noreply.github.com>
2024-06-28 15:52:57 +0700

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
+AOS.init();
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
-
 const textArray = ["Front-End Developer", "Back-End Developer", "Web Developer","I Love Coding!"];
 const typingDelay = 200;
 const erasingDelay = 100;
@@ -41,65 +41,79 @@ function erase() {
 $(document).ready(function() {
  
   
-  $(".aboutme").hide();
-  $(".myimg").hide();
-$(".thisimg").hide();
-  $("#skillset").hide();
-  $(".skiii").hide();
-  $("#web").hide();
-  $("#Machine").hide()
-  $("#python").hide()
-  $("#tooliuse").hide()
-  $("#toool").hide()
+  // $(".aboutme").hide();
+  // $(".myimg").hide();
+  // $(".thisimg").hide()
+  // $("#skillset").hide();
+  // $(".skiii").hide();
+  // $("#web").hide();
+  // $("#Machine").hide()
+  // $("#python").hide()
+  // $("#tooliuse").hide()
+  // $("#toool").hide()
   VanillaTilt.init(document.querySelector(".myimg img"), {
 		max: 15,
 		speed: 1000
 	});
-	
-	//It also supports NodeList
-	VanillaTilt.init(document.querySelectorAll(".myimg img"));
-    if (textArray.length) setTimeout(type, newTextDelay + 250);
-    $(window).scroll(function() {
-      var scrollPosition = $(this).scrollTop();
-      var vh = $(window).height()
+	$(window).scroll(function() {
+    var scrollPosition = $(this).scrollTop();
+       var vh = $(window).height()
         if (scrollPosition > 0) {
             $(".navv").addClass("sticky");
         } else {
-            $(".navv").removeClass("sticky");
-        }
-        if (scrollPosition > 0.4 *vh && scrollPosition<1.9*vh) {
-          $(".aboutme").fadeIn();
-          setTimeout(function(){
-            $(".myimg").fadeIn();
-		  $(".thisimg").fadeIn();
-          },200)
-        }
-        if(scrollPosition>1.1*vh && scrollPosition<3.3*vh){
-         $("#skillset").fadeIn()
-         setTimeout(function(){
-          $("#web").fadeIn();
-          setTimeout(function(){
-            $(".skiii").fadeIn()
-            setTimeout(function(){
-              $("#Machine").fadeIn()
-              setTimeout(function(){
-                $("#python").fadeIn()
-                setTimeout(function(){
-                  $("#tooliuse").fadeIn()
-                  setTimeout(function(){
-                    $("#toool").fadeIn()
-                  },300);
-                },300);
-              },300);
-            },300);
-          },300);
-        },300)
+             $(".navv").removeClass("sticky");
+       }
+  })
+	//It also supports NodeList
+
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+    // $(window).scroll(function() {
+    //   var scrollPosition = $(this).scrollTop();
+    //   var vh = $(window).height()
+    //     if (scrollPosition > 0) {
+    //         $(".navv").addClass("sticky");
+    //     } else {
+    //         $(".navv").removeClass("sticky");
+    //     }
+    //     if (scrollPosition > 0.4 *vh && scrollPosition<1.9*vh) {
+    //       $(".aboutme").fadeIn();
+    //       setTimeout(function(){
+    //         $(".myimg").fadeIn();
+    //         $(".thisimg").fadeIn()
+    //         VanillaTilt.init(document.querySelectorAll(".myimg img"));
+    //       },200)
+    //     }
+    //     else{
+    //       $(".aboutme").hide();
+    //       $(".myimg").hide();
+    //       $(".thisimg").hide()
+    //     }
+    //     if(scrollPosition>1.1*vh && scrollPosition<3.3*vh){
+    //      $("#skillset").fadeIn()
+    //      setTimeout(function(){
+    //       $("#web").fadeIn();
+    //       setTimeout(function(){
+    //         $(".skiii").fadeIn()
+    //         setTimeout(function(){
+    //           $("#Machine").fadeIn()
+    //           setTimeout(function(){
+    //             $("#python").fadeIn()
+    //             setTimeout(function(){
+    //               $("#tooliuse").fadeIn()
+    //               setTimeout(function(){
+    //                 $("#toool").fadeIn()
+    //               },300);
+    //             },300);
+    //           },300);
+    //         },300);
+    //       },300);
+    //     },300)
        
-        }
-        else{
+    //     }
+    //     else{
         
-        }
-    });
+    //     }
+    // });
     $(".navbar-toggler").click(function(){
         $(".navv").toggleClass("sticky");
     })
